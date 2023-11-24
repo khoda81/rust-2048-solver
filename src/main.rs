0b532a09271e3e29b2647bf3bf42daf0054b8450
khoda81 <20.mahdikh.0@gmail.com>
2023-11-24 23:11:35 +0330

    Removing initialization comment from main

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,12 @@ fn main() {
     // show_map(heuristic::get_lookup());
 
     let mut game = Game::<4, 4>::create();
-    // game.board = [
-    //     // BOARD
-    //     [3, 3, 1, 1],
-    //     [1, 9, 5, 0],
-    //     [10, 2, 7, 4],
-    //     [6, 1, 6, 8],
-    // ]
-    // .into();
-
     let mut ai = DFS::new();
 
     loop {
         println!("{}", game.board);
 
-        let timeout = Duration::from_secs_f64(0.2);
+        let timeout = Duration::from_secs_f64(1.0);
         let deadline = Instant::now() + timeout;
 
         let action = ai.act(&game.board, deadline);
